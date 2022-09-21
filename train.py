f476e601c45008e72f31e72275b1bfa1134e764a
HYUN WOOK KANG <kangtaepoong@naver.com>
2022-09-22 07:54:32 +0900

    Update train.py

--- train.py
+++ train.py
@@ -108,37 +108,6 @@ class MSE(nn.Module):
 
         return mse
 
-class CMD(nn.Module):
-    """
-    Adapted from https://github.com/wzell/cmd/blob/master/models/domain_regularizer.py
-    """
-
-    def __init__(self):
-        super(CMD, self).__init__()
-
-    def forward(self, x1, x2, n_moments):
-        mx1 = torch.mean(x1, 0)
-        mx2 = torch.mean(x2, 0)
-        sx1 = x1-mx1
-        sx2 = x2-mx2
-        dm = self.matchnorm(mx1, mx2)
-        scms = dm
-        for i in range(n_moments - 1):
-            scms += self.scm(sx1, sx2, i + 2)
-        return scms
-
-    def matchnorm(self, x1, x2):
-        power = torch.pow(x1-x2,2)
-        summed = torch.sum(power)
-        sqrt = summed**(0.5)
-        return sqrt
-        # return ((x1-x2)**2).sum().sqrt()
-
-    def scm(self, sx1, sx2, k):
-        ss1 = torch.mean(torch.pow(sx1, k), 0)
-        ss2 = torch.mean(torch.pow(sx2, k), 0)
-        return self.matchnorm(ss1, ss2)
-
 if __name__ == '__main__':
     
     ap = argparse.ArgumentParser()
@@ -202,10 +171,6 @@ if __name__ == '__main__':
     num_trials = 1
     
     criterion = nn.MSELoss(reduction="mean")
-
-    # loss_diff = DiffLoss()
-    loss_recon = MSE()
-    loss_cmd = CMD()
     
     best_valid_loss = float('inf')
     lr_scheduler = torch.optim.lr_scheduler.ExponentialLR(optimizer, gamma=0.5)
@@ -219,7 +184,6 @@ if __name__ == '__main__':
         model.train()
 
         train_loss_cls, train_loss_sim, train_loss_diff = [], [], []
-        train_loss_recon = []
         train_loss_sp = []
         train_loss = []
         #in train func
